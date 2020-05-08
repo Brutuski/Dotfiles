f6edaa88bb435bf5ac49e31eb43470f77e9db537
adhiraj.sirohi@gmail.com <adhiraj.sirohi@gmail.com>
2020-05-08 16:18:44 +0200

    Updated icons
    icons changed to FontAwesome

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-MENU="$(rofi -theme ~/.config/rofi/themes/dracula.rasi -sep "|" -dmenu -i -p 'System' -location 0 -width 20 -font 'Hack Bold Nerd Front 12' -separator-style 'dotted'  -hide-scrollbar -line-padding 5 -padding 20 -lines 3 <<< "⭘ Logout|🔃 Reboot|⏻ Shutdown")"
+MENU="$(rofi -theme ~/.config/rofi/themes/dracula.rasi -sep "|" -dmenu -i -p 'System' -location 0 -width 20 -font 'Hack Bold Nerd Front 12' -separator-style 'dotted'  -hide-scrollbar -line-padding 5 -padding 20 -lines 3 <<< " Logout| Reboot| Shutdown")"
 
 case "$MENU" in
   *Logout) i3lock -i ~/Documents/Chrome-Downloads/Lockscreen.png -p default -n ;;
