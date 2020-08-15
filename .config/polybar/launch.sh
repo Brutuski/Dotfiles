99ec01beb041ec03234488971292603f72ddf5bd
lighthaus-theme <adhiraj.sirohi@outlook.com>
2020-08-15 13:30:19 +0200

    Lighthaus theme implemented + New look
    Lighthaus theme + deleted unnecessary scripts + new icons + edited polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-
-## Add this to your wm startup file.
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
--- wclock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-#Script to display current time in India and New York in Dunst
-
-Delhi=$(date -d '+ 270 minutes' '+ %H:%M')
-dunstify -t 10000 "Time in Delhi" "$Delhi"
-
-NewYork=$(date -d '- 300 minutes' '+ %H:%M')
-dunstify -t 10000 "Time in New York" "$NewYork"
