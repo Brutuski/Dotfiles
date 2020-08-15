cddf28dfc849b9b940cef01e7b567a000605a4be
lighthaus-theme <adhiraj.sirohi@outlook.com>
2020-08-15 13:54:31 +0200

    Lighthaus theme + cleanup
    Lighthaus theme colors + cleaned up config + removed unnecessary files

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -1,6 +1,6 @@
-#!/bin/zsh
+#!/bin/sh
 
-MENU="$(rofi -theme ~/.config/rofi/themes/dracula.rasi -sep "|" -dmenu -i -p 'System' -location 0 -width 20 -font 'Hack Bold Nerd Front 12' -separator-style 'dotted'  -hide-scrollbar -line-padding 5 -padding 20 -lines 3 <<< " Logout| Reboot| Shutdown")"
+MENU="$(rofi -theme ~/.config/rofi/themes/lighthaus.rasi -sep "|" -dmenu -i -p 'System' -font 'Source Code Pro Medium 12' <<< " Logout| Reboot| Shutdown")"
 
 case "$MENU" in
   *Logout) i3lock -i ~/Documents/Chrome-Downloads/Lockscreen.png -p default -n ;;
