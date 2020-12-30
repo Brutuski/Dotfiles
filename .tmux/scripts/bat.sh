93e9be2e798e49d8995af2eb064797835bcb1f52
lighthaus-theme <adhiraj.sirohi@outlook.com>
2020-12-30 17:30:19 +0100

    Tmux Scripts modified
    context based battery icons added + vpn script deleted

--- .tmux/scripts/bat.sh
+++ .tmux/scripts/bat.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Please enter the corrent BAT<num>
+status=$(cat /sys/class/power_supply/BAT1/status)
+level=$(cat /sys/class/power_supply/BAT1/capacity)
+
 if [ -d /sys/class/power_supply/BAT1 ];
 then
-    now=`cat /sys/class/power_supply/BAT1/capacity`
-    printf "Bat: %.0f%%" $now
+    if [ "$status"  = "Discharging" ];then
+         printf "🔋%.0f%%" $level
+    elif [ "$status"  = "Charging" ];then
+         printf "⚡%.0f%%" $level
+    else
+         printf "🔌%.0f%%" $level
+    fi
 else
     echo ""
 fi
